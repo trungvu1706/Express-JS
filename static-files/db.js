0a2adf009b4aae65c9d4f2a2ae68c6c2e3dea1ba
TrungVu <trungvu1706@gmail.com>
2020-12-03 23:35:10 +0700

    learn static-files

---
+++ static-files/db.js
@@ -0,0 +1,11 @@
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+var db = low(adapter);
+
+db.defaults({ books: [] }).write();
+db.defaults({ users: [] }).write();
+db.defaults({ transactions: [] }).write();
+
+
+module.exports = db;
