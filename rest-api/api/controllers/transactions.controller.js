b71a86d239f14c15da78aefce64aae4743e897a8
TrungVu <trungvu1706@gmail.com>
2020-12-23 11:30:28 +0700

    api for transactions

---
+++ rest-api/api/controllers/transactions.controller.js
@@ -0,0 +1,20 @@
+var User = require('../../models/user.model');
+var Book = require('../../models/book.model');
+var Transaction = require('../../models/transaction.model');
+
+
+module.exports.index = async function(req, res) {
+    try {
+        var transactions = await Transaction.find()
+            .populate({ path: 'user', select: 'name avatar' })
+            .populate({ path: 'books.book' });
+
+        //console.log(req.user)
+
+        res.json(transactions);
+
+    } catch (error) {
+        console.log(error);
+    }
+
+};
