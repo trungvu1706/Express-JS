e84ca7016233f5d6b86c6780d4e8e53f98d06397
TrungVu <trungvu1706@gmail.com>
2021-01-23 23:02:56 +0700

    fix code to query data at getBooks func

--- rest-api/index.js
+++ rest-api/index.js
@@ -1,51 +1,53 @@
-require('dotenv').config();
+require("dotenv").config();
 
-var express = require('express');
+var express = require("express");
+var cors = require("cors");
 var app = express();
-var path = require('path');
-var cookieParser = require('cookie-parser');
+var path = require("path");
+var cookieParser = require("cookie-parser");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URL);
 
-var bookRoute = require('./routers/book.route');
-var userRoute = require('./routers/user.route');
-var transactionRoute = require('./routers/transaction.route');
-var homeRoute = require('./routers/home.route');
-var authRoute = require('./routers/auth.route');
-var cartRoute = require('./routers/cart.route');
+// var bookRoute = require('./routers/book.route');
+var userRoute = require("./routers/user.route");
+var transactionRoute = require("./routers/transaction.route");
+var homeRoute = require("./routers/home.route");
+var authRoute = require("./routers/auth.route");
+var cartRoute = require("./routers/cart.route");
 
-var apiTransactionRoute = require('./api/routers/transactions.router');
-var apiAuth = require('./api/routers/auth.router');
+var apiTransactionRoute = require("./api/routers/transactions.router");
+var apiAuth = require("./api/routers/auth.router");
+var bookRoute = require("./api/routers/book.router");
 
-var authMiddleware = require('./middlewares/auth.middleware');
-var sessionMiddleware = require('./middlewares/session.middleware');
+var authMiddleware = require("./middlewares/auth.middleware");
+var sessionMiddleware = require("./middlewares/session.middleware");
 
 var port = process.env.PORT || 3000;
 
-app.set('view engine', 'pug');
-app.set('views', path.resolve(__dirname, './views'));
+app.set("view engine", "pug");
+app.set("views", path.resolve(__dirname, "./views"));
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cors());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(cookieParser(process.env.COOKIE_SECRET_KEY));
 app.use(sessionMiddleware);
 
-
-app.use('/books', bookRoute);
-app.use('/users', authMiddleware.requireAuth, userRoute);
-app.use('/transactions', authMiddleware.requireAuth, transactionRoute);
-app.use('/home', authMiddleware.requireAuth, homeRoute);
+// app.use('/books', bookRoute);
+app.use("/books", bookRoute);
+app.use("/users", authMiddleware.requireAuth, userRoute);
+app.use("/transactions", authMiddleware.requireAuth, transactionRoute);
+app.use("/home", authMiddleware.requireAuth, homeRoute);
 // app.use('/auth', authRoute);
-app.use('/cart', cartRoute);
+app.use("/cart", cartRoute);
 
-// from Api folder 
-app.use('/api/transactions', apiTransactionRoute);
-app.use('/api/auth', apiAuth);
+// from Api folder
+app.use("/api/transactions", apiTransactionRoute);
+app.use("/api/auth", apiAuth);
 
-
-app.listen(port, function() {
-    console.log('Server listening on port ' + port);
+app.listen(port, function () {
+  console.log("Server listening on port " + port);
 });
